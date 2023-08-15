5a56097921c6e2fcb4951ccc57d7e0adc120821c
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2023-08-15 14:04:30 +0300

    Lottery contract excercise

---
+++ Lottary/Owner.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+/* Owner contract, which specifies owner functionality. (to be inferited)
+*/
+contract Owner{
+    address public owner;
+
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Unauthorized");
+        _;
+    }
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function changeOwner(address _owner) onlyOwner external {
+        owner = _owner;
+    }
+
+}
