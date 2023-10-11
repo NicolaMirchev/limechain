a0a0093e121179c58f44e8abcbca93b3dc160b82
NikolaMirchev <nicola.mirchev@gmail.com>
2023-10-11 09:11:33 +0300

    Context excercise

---
+++ Go/Excercises/Context/context.go
@@ -0,0 +1,25 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+
+type Store interface {
+	Fetch(context.Context) (string, error);
+	Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: log error however you like
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
